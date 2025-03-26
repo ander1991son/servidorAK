854aab0fe20aae3cb3119438ab5f4864854198c4
ander1991son <usuariopalacio72@gmail.com>
2025-03-26 16:32:58 -0500

    servidor render

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "¡Hola, Render! Tu servidor Flask está funcionando."
+    return send_file('index.html')  # Envía el archivo desde la raíz
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+
+
+
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return "¡Hola, Render! Tu servidor Flask está funcionando."
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
